d745b642c890f3704ee5d5ab44234d587e618828
Ghousebee <152587830+Ghousebee@users.noreply.github.com>
2023-12-26 10:40:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal(".about__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+var modal = document.getElementById('id01');
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
